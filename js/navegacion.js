31c136777f2be3340f7b402cf669d7cddd580843
damian othacehe <damianothacehe@gmail.com>
2024-06-20 13:32:26 -0300

    creacion de la barra de navegacion

---
+++ js/navegacion.js
@@ -0,0 +1,25 @@
+const cabecera = document.getElementById("header");
+const navegacion = document.createElement("navbar");
+const nav = document.createElement("nav");
+const ul = document.createElement("ul");
+const imagen = document.createElement("img");
+const a = document.createElement("a");
+
+cabecera.appendChild(navegacion);
+navegacion.appendChild(nav);
+nav.appendChild(ul);
+
+navegacion.className = "navbar";
+
+const pages = ["Logo", "Index", "Carrito", "Contacto"];
+
+for(let i = 0; i<pages.length; i++){
+    const li = document.createElement("li");
+    if(i === 0){li.innerHTML = `<a href="index.html"><img src="./assets/img/logo-sin-fondo.png" alt="Inicio" style = "width: 50px; height: 50px;"></a>`
+    }
+    else{li.innerHTML = `<a href="${pages[i].toLowerCase()}.html">${pages[i]}</a>`
+    };
+    ul.appendChild(li);
+};
+
+
